245af28ce7d5dc8e198da62bf080c59bf73b4d6c
skycloudluxe <info@skycloudluxe.com>
2025-06-08 17:15:37 -0700

    Delete index.js

--- index.js
+++
@@ -1,1 +0,0 @@
-import React from 'react'; import Component from '../components/component'; const Index = () => {   return (     <>       <style>{`         body {           margin: 0;         }       `}</style>       <Component />     </>   ); }; export default Index;
